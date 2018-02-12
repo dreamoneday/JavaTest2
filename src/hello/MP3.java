5a61dab97459763338433143a356511628d79033
dreamonce <m18908610240@163.com>
2018-02-12 16:32:27 +0800

    second commit

---
+++ src/hello/MP3.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class MP3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/hello/Main.java
@@ -0,0 +1,9 @@
+package hello;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer n1 = new Integer(47);
+        Integer n2 = new Integer(47);
+        System.out.println(n1 == n2);
+    }
+}
